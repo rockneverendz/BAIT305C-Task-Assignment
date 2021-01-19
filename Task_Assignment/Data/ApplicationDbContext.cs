c53776d1126a606fde3be40517e82a5097d9fd6b
VerniyLust <31733763+rockneverendz@users.noreply.github.com>
2021-01-20 02:32:07 +0800

    Add project files.

---
+++ Task_Assignment/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Task_Assignment.Models;
+
+namespace Task_Assignment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Task_Assignment/Models/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task_Assignment.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Position Position { get; set; }
+        public Team Team { get; set; }
+        public Status Status { get; set; }
+        public string Security { get; set; }
+    }
+
+    public enum Position
+    {
+        Director,
+        HumanResource,
+        Finance,
+        Admin,
+        Manager,
+        QualityAssuranceEngineer,
+        SolutionArchitect,
+        BusinessAnalysis,
+        ArtDirector,
+        TeamLead,
+        SeniorSoftwareEngineer,
+        UIEngineer,
+        SupportEngineer,
+        InfrastructureSupportEngineer,
+    }
+
+    public enum Team
+    {
+        Operation,
+        ProjectManagement,
+        Architect,
+        Designer,
+        Development,
+        InfrastructureSupport,
+        ProductionSupport,
+        QualityAssurance,
+    }
+
+    public enum Status
+    {
+        Active,
+        Disabled,
+        Suspended,
+    }
+}
