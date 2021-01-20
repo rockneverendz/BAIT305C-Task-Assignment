ab70c1dfec206fe5300815eedb3511d392a4be96
VerniyLust <31733763+rockneverendz@users.noreply.github.com>
2021-01-21 02:48:29 +0800

    Create EmployeesController.cs
    Modifiy scaffolded code
    Update Create method
    Add remote methods, IsUserNameUnique and IsEmployeeIDUnique

---
+++ Task_Assignment/Controllers/EmployeesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Task_Assignment.Data;
+using Task_Assignment.Models;
+using Task_Assignment.ViewModels;
+
+namespace Task_Assignment.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        [AllowAnonymous]
+        public JsonResult IsUserNameUnique(string Username)
+        {
+            bool isUnique = !db.Employees.Any(e => e.Username == Username);
+            return Json(isUnique, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public JsonResult IsEmployeeIDUnique(long EmployeeID)
+        {
+            bool isUnique = !db.Employees.Any(e => e.EmployeeID == EmployeeID);
+            return Json(isUnique, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,Username,Email,FullName,Password,ComfirmPassword,JoinDate,Position,Team,Security")] CreateEmployee createEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = createEmployee.ToEmployee();
+
+                employee.Status = Status.Active;
+                employee.IPAddress = Request.UserHostAddress;
+
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(createEmployee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,Username,Email,FullName,Password,JoinDate,Position,Team,Status,Security")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
